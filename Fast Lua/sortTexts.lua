b354f135055e3555201a307f80432ce305bde950
Peter Mac Gaul <pedromacgaul@gmail.com>
2020-07-27 19:31:05 -0300

    Emprolijando sortText

--- Fast Lua/sortTexts.lua
+++ Fast Lua/sortTexts.lua
@@ -1,20 +1,17 @@
+
 function compare(a,b)
     return a[1] < b[1]
 end
 
 
-function string:split(delimiter)
+function string:split( line )
   local result = { }
-  local from  = 1
-  local delim_from, delim_to = string.find( self, delimiter, from  )
-  while delim_from do
-    table.insert( result, string.sub( self, from , delim_from-1 ) )
-    from  = delim_to + 1
-    delim_from, delim_to = string.find( self, delimiter, from  )
+  for token in string.gmatch(line , "[^%s]+") do
+     result[token] = token
   end
-  table.insert( result, string.sub( self, from  ) )
   return result
 end
+
 
 function sortFile(filename)
     print("----------------------------")
@@ -26,42 +23,58 @@ function sortFile(filename)
 
     local words = {}
     local word_freq_table = {}
-    local words_freq = {}
     local word_count = 0
 
     local line = file:read("*line")
 
     while line ~= nil do
-    	local t = line:split(" ")
+    	local t = line:split( line )
     	word_count = word_count + #t
     	for v, w in pairs(t) do
+
     		table.insert( words, w )
-            word_freq_table[w] = word_freq_table[w] or 0
-            word_freq_table[w] = word_freq_table[w] + 1
+
+            if word_freq_table[w] ~= nil then
+                word_freq_table[w] = word_freq_table[w] + 1
+            else
+                word_freq_table[w] = 1
+            end
+
     	end
       	line = file:read("*line")
     end
 
     table.sort(words)
 
-    for k, v in pairs(word_freq_table) do
-        table.insert( words_freq, {key = k, value = v} )
-    end
-
-    table.sort(words_freq, function(a,b) return a.value < b.value end)
-
-    words_freq_namefile = "WordsFreq_" .. filename
-    words_file = io.open(words_freq_namefile, "w")
-
-    for k,v in pairs(words_freq) do
-        words_file:write( v.value, " " , v.key, "\n" )
-    end
+    -- Sort file
 
     sorted_name = "Sorted_" .. filename
     output_file = io.open(sorted_name, "w")
 
     for i, l in ipairs(words) do
-    	output_file:write( l, "\n" )
+        output_file:write( l, "\n" )
+    end
+
+
+    -- Word frequency
+
+    local words_freq = {}
+    -- Las palabras son las claves
+    -- Entonces para ordenarlas tengo que tenerlas por valor
+    -- Para esto lo hacemos definiendo una tabla con nuestros clave/valor
+
+    for k, v in pairs( word_freq_table ) do
+        table.insert( words_freq, {key = k, value = v} )
+    end
+
+    -- Orderno por valor
+    table.sort(words_freq, function(a,b) return a.value < b.value end)
+
+    words_freq_namefile = "WordsFreq_" .. filename
+    words_file = io.open(words_freq_namefile, "w")
+
+    for k,v in pairs( words_freq ) do
+        words_file:write( v.value, " " , v.key, "\n" )
     end
 
     print( "Word counter : ", word_count )
@@ -69,6 +82,7 @@ function sortFile(filename)
     file:close()
     output_file:close()
     words_file:close()
+
     end_time = os.clock()
 
     print( "Elapsed time : ", end_time - start_time )
